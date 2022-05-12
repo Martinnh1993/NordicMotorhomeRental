38ea4f09645a0f47005ed0b5ec9fc7a2c867a5e8
Martinnh1993 <90694207+Martinnh1993@users.noreply.github.com>
2022-05-12 12:38:48 +0200

    Initial commit

---
+++ NordicMotorhomeRental/src/test/java/com/KEA/NordicMotorhomeRental/NordicMotorhomeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KEA.NordicMotorhomeRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicMotorhomeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
